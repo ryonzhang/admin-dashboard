50541b3b67d9f7ba5fdb20aa252aa18c8643e526
RUIYANG ZHANG <zhangruiyang36@gmail.com>
2020-04-17 12:54:01 +0800

    add intl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Main} from "./pages/Main/Main";
+import { IntlProvider, FormattedMessage } from "react-intl";
+import {textPool} from "./res/languages/lang";
+
 export default function App(props) {
   return (
-      <Main/>
+      <IntlProvider locale={'en'} messages={textPool['es_CL']}>
+        <Main/>
+      </IntlProvider>
   );
 }
 
--- src/pages/UserManagementList/UserManagementList.tsx
+++ src/pages/UserManagementList/UserManagementList.tsx
@@ -5,26 +5,28 @@ import {TabItem} from "../../components/Tab/TabItem/TabItem";
 import {Tab} from "../../components/Tab/Tab";
 import {Table} from "../../components/Table/Table";
 import {USER_MANAGEMENT_PAGES} from "../UserManagement/UserManagement";
+import {FormattedMessage } from "react-intl";
+import {TEXT_ID} from '../../res/languages/lang'
 
 type UserManagementProps = {
     onSetActivePage: Function,
 }
 
 export const UserManagementList: FunctionComponent<UserManagementProps> = ({onSetActivePage}) =>
-        <div className='user-management'>
-            <div className='user-management-title'>
-                <text className='user-management-title-text'><b>User Management</b></text>
-                <button className='user-management-invite-btn' onClick={()=>{onSetActivePage(USER_MANAGEMENT_PAGES.INVITE_USERS)}}>
-                    INVITE NEW USERS
-                </button>
-            </div>
-            <div className='user-management-content'>
-                <div className='user-management-panel'>
-                    <Tab>
-                        <TabItem text='Active Users' selected={false}/>
-                        <TabItem text='Pending Users' selected={true}/>
-                    </Tab>
-                    <Table indexed={true} headers={['FULL NAME','EMAIL','ROLE','LAST LOGIN','TIME']} rows={[['Admin ClaroChile','admin.clarochile.staging@juvo.com','Admin','04/16/2020','06:21']]} actionText={'EDIT'}/>
-                </div>
+    <div className='user-management'>
+        <div className='user-management-title'>
+            <text className='user-management-title-text'><b><FormattedMessage id={TEXT_ID.USER_MANAGEMENT} /></b></text>
+            <button className='user-management-invite-btn' onClick={()=>{onSetActivePage(USER_MANAGEMENT_PAGES.INVITE_USERS)}}>
+                INVITE NEW USERS
+            </button>
+        </div>
+        <div className='user-management-content'>
+            <div className='user-management-panel'>
+                <Tab>
+                    <TabItem text='Active Users' selected={false}/>
+                    <TabItem text='Pending Users' selected={true}/>
+                </Tab>
+                <Table indexed={true} headers={['FULL NAME','EMAIL','ROLE','LAST LOGIN','TIME']} rows={[['Admin ClaroChile','admin.clarochile.staging@juvo.com','Admin','04/16/2020','06:21']]} actionText={'EDIT'}/>
             </div>
         </div>
+    </div>
